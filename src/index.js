17cbbba1052544c00bfd0e1e27253bb30e215b55
furuhama <furuhama@users.noreply.github.com>
2018-10-14 15:06:57 +0900

    Set stateNumber & jumpTo method

--- src/index.js
+++ src/index.js
@@ -73,6 +73,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
@@ -87,7 +88,7 @@ class Game extends React.Component {
   // 最適化も行いやすくなるみたい
   // https://reactjs.org/docs/optimizing-performance.html#examples
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     // history や current 全体をコピーすることはせずに
     // 最低限の配列のみをコピーする
@@ -106,13 +107,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
